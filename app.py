8020a55272d00fe1d937b5322b3d1fcd8ed5699f
Sergio <seregaant71@gmail.com>
2024-09-23 21:23:05 +0300

    Добавил ориджин

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/send_message": {"origins": "https://your-allowed-origin.com"}})
+CORS(app, resources={r"/send_message": {"origins": "https://sergiosboy.github.io/date/"}})
 # Настройка логирования
 logging.basicConfig(level=logging.INFO)
 
